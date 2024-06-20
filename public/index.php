09b522c5372f2c076ca406815548afd153750b9f
Oli <81637846+oliiver185@users.noreply.github.com>
2024-06-20 11:07:21 +0100

    first commit

---
+++ public/index.php
@@ -0,0 +1,32 @@
+<!doctype html>
+
+<html lang="en">
+  <head>
+    <title>Globe Bank</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="./stylesheets/staff.css" media="all" />
+  </head>
+
+  <body>
+
+  <header>
+    <h1>GBA staff area</h1>
+  </header>
+
+  <navigation>
+    <ul>
+      <li>
+        <a href="index.php">Menu</a>
+      </li>
+    </ul>
+  </navigation>
+
+
+  <footer>
+
+  &copy: <?php  echo date('Y'); ?> Globe Bank
+  </footer>
+
+  <!-- <script src="https://cdn.tailwindcss.com"></script>xs -->
+  </body>
+</html>
